4c8ad61cec165b6d4a6a57f7e9840bc778ca41f6
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-02-11 12:03:33 +0100

    feat: fetch a book by id
    Implemeted an endpoint to fetch a book by it's id and return the right error when not found

--- api/routes/books.py
+++ api/routes/books.py
@@ -2,6 +2,7 @@ from typing import OrderedDict
 
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+from fastapi import HTTPException, status
 
 from api.db.schemas import Book, Genre, InMemoryDB
 
@@ -60,3 +61,14 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Book with id {book_id} not found"
+        )
+    return book
